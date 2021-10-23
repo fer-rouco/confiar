8266196e3162dc6a8934a6fe10af3e95e4ee5d01
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2021-10-23 12:13:01 -0300

    Fix in Customers page

--- client/src/pages/customers.jsx
+++ client/src/pages/customers.jsx
@@ -1,62 +1,63 @@
-import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useAlertMessage } from '../contexts/alert-message-context';
-import { useDialog } from '../contexts/dialog-context';
 import { removeCustomer, findCustomers } from '../services/customer-service';
 import Panel from '../components/containers/panel';
 import Table from '../components/table/table';
 import { removeColumnDefinition, textColumnDefinition } from '../components/table/column-definitions/column-definition';
+// import Form from '../components/containers/form';
+// import TextField from '../components/controls/fields/input/text-field';
+
+// TODO: Filtros
+// const [NAME] =
+//   [
+//     { id: 'name', label: 'Nombre' }
+//   ];
+
 
 export default function Customers() {
   const history = useHistory();
-  const dialog = useDialog();
   const { addSuccessMessage, addErrorMessage } = useAlertMessage();
-  const [columnDefinitions, setColumnDefinitions] = useState(null);
 
   function create() {
     history.push('/Customer');
   };
 
-  useEffect(() => {
-    setColumnDefinitions(
-      [
-        textColumnDefinition({
-          key: 'name',
-          label: 'Nombre',
-          target: '/Customer'
-        }),
-        textColumnDefinition({
-          key: 'lastName',
-          label: 'Apellido'
-        }),
-        textColumnDefinition({
-          key: 'mail', 
-          label: 'E-Mail'
-        }),
-        removeColumnDefinition({
-          key: 'remove',
-          icon: 'trash-fill',
-          dialogConfig: {
-            title: 'Eliminar Cliente',
-            message: 'Esta seguro que desea eliminar el cliente <%NAME%>?',
-            onAccept: (model) => {
-              return removeCustomer(model.id)
-                .then(() => {
-                  addSuccessMessage(
-                    'El cliente ' + model.name + ' fue eliminado exitosamente.',
-                  );
-                })
-                .then((errorData) => {
-                  if (errorData) {
-                    addErrorMessage(errorData.message);
-                  }
-                });
-            }
-          }
-        })
-      ]
-    );
-  }, [setColumnDefinitions]);
+  let columnDefinitions = [
+    textColumnDefinition({
+      key: 'name',
+      label: 'Nombre',
+      target: '/Customer'
+    }),
+    textColumnDefinition({
+      key: 'lastName',
+      label: 'Apellido'
+    }),
+    textColumnDefinition({
+      key: 'mail', 
+      label: 'E-Mail'
+    }),
+    removeColumnDefinition({
+      key: 'remove',
+      icon: 'trash-fill',
+      dialogConfig: {
+        title: 'Eliminar Cliente',
+        message: 'Esta seguro que desea eliminar el cliente <%NAME%>?',
+        onAccept: (model) => {
+          return removeCustomer(model.id)
+            .then(() => {
+              addSuccessMessage(
+                'El cliente ' + model.name + ' fue eliminado exitosamente.',
+              );
+            })
+            .then((errorData) => {
+              if (errorData) {
+                addErrorMessage(errorData.message);
+              }
+            });
+        }
+      }
+    })
+  ];
 
   return (
     <Panel
@@ -72,6 +73,14 @@ export default function Customers() {
         },
       ]}
     >
+      {/* TODO: Filtros */}
+      {/* <Form>
+        <div className="row">
+          <div className="col-md-3">
+            <TextField attr={NAME.id} label={NAME.label} ></TextField>
+          </div>
+        </div>
+      </Form> */}
       <Table columnDefinitions={columnDefinitions} requestRowObjectsFunction={findCustomers} ></Table>
     </Panel>
   );

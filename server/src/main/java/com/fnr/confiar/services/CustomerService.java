72c97e54a0c8385cd0078f2c251ea25551a13704
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2021-10-28 17:21:23 -0300

    Cleaning in CustomerService

--- server/src/main/java/com/fnr/confiar/services/CustomerService.java
+++ server/src/main/java/com/fnr/confiar/services/CustomerService.java
@@ -4,14 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 import com.fnr.confiar.entities.Customer;
-import com.fnr.confiar.models.CustomerModel;
 import com.fnr.confiar.repositories.CustomerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,35 +25,6 @@ public class CustomerService extends BaseService<Customer> {
 
   public List<Customer> findAll() {
     return (List<Customer>) customerRepository.findAll();
-  }
-  
-  public List<Customer> findCustomersByFilters(int pageFrom, int pageSize, String nameParam, String lastNameParam, String mailParam) {
-    String name = String.format(QueryConstants.LIKE, (nameParam != null) ? nameParam.toLowerCase(): "");
-    String lastName = String.format(QueryConstants.LIKE, (lastNameParam != null) ? lastNameParam.toLowerCase(): "");
-    String mail = String.format(QueryConstants.LIKE, (mailParam != null) ? mailParam.toLowerCase(): "");
-
-    CriteriaBuilder cb = em.getCriteriaBuilder();
-    CriteriaQuery<Customer> cq = cb.createQuery(Customer.class);
-
-    Root<Customer> book = cq.from(Customer.class);
-    Predicate namePredicate = cb.like(cb.lower(book.get(CustomerModel.Fields.name)), name);
-    Predicate lastNamePredicate = cb.like(cb.lower(book.get(CustomerModel.Fields.lastName)), lastName);
-    Predicate mailPredicate = cb.like(cb.lower(book.get(CustomerModel.Fields.mail)), mail);
-  
-    cq.where(namePredicate, lastNamePredicate, mailPredicate);
-
-    TypedQuery<Customer> query = em.createQuery(cq);
-    return query.getResultList();
-
-
-    // return (List<Customer>) Customer.reduce(
-    //   customerRepository.findAllByNameOrLastNameOrMailLikeIgnoreCase(
-    //     PageRequest.of(pageFrom, pageSize, Sort.by(Sort.Direction.ASC, CustomerModel.Fields.name)),
-    //     name,
-    //     lastName,
-    //     mail
-    //   )
-    // );
   }
 
   public Customer save(Customer userModel) {
